5af307b27cb6787f9f1b85ca2c5ea4d0c10270ed
thsdmfwns <son2044@gmail.com>
2021-02-05 09:48:20 +0900

    #

--- PicVicomSample/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Chat.razor.g.cs
+++ PicVicomSample/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Chat.razor.g.cs
@@ -13,91 +13,91 @@ namespace PicVicomSample.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 1 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 2 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 3 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 4 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 5 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 6 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 7 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 8 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 9 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using PicVicomSample.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 10 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using PicVicomSample.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 12 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using MatBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\_Imports.razor"
+#line 13 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\_Imports.razor"
 using BlazorStrap;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\Pages\Chat.razor"
+#line 2 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\Pages\Chat.razor"
 using Microsoft.AspNetCore.SignalR.Client;
 
 #line default
@@ -112,7 +112,7 @@ using Microsoft.AspNetCore.SignalR.Client;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 29 "C:\Users\dnfls\OneDrive\바탕 화면\PicVicomSample\PicVicomSample\Client\Pages\Chat.razor"
+#line 29 "C:\Users\son20\PicVicomSample\PicVicomSample\Client\Pages\Chat.razor"
        
     private HubConnection hubConnection;
     private List<string> messages = new List<string>();
