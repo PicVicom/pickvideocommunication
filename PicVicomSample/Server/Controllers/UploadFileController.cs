297d0e187abebae2ca4eb05aea7ba43c99f33561
thsdmfwns <son2044@gmail.com>
2021-02-08 01:25:54 +0900

    영상업로드 추가

---
+++ PicVicomSample/Server/Controllers/UploadFileController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PicVicomSample.Server.StreamingClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PicVicomSample.Server.Controllers
+{
+    [Route("upload")]
+    [ApiController]
+    public class UploadFileController : ControllerBase
+    {
+        [HttpPost("{roomid}/{owner}")]
+        public async Task<IActionResult> Single(IFormFile file, int roomid, string owner)
+        {
+            try
+            {
+                var f = new UploadFIle(file);
+                if (f.CheckIfExcelFile())
+                {
+                    await f.WriteFile();
+                }
+
+                var streaminginfo = new StreamingInfo { FileName = f.Name, Owner = owner, RoomID = roomid };
+
+                if (!Streaming.Instance.StreamingQue.ContainsKey(streaminginfo.RoomID))
+                {
+                    await Streaming.Instance.AddRoom(streaminginfo.RoomID);
+                }
+
+                Streaming.Instance.EnQue(streaminginfo.RoomID, streaminginfo);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ PicVicomSample/Server/UploadFile.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PicVicomSample.Server
+{
+    public class UploadFIle
+    {
+        public UploadFIle(IFormFile file)
+        {
+            File = file;
+            OriginalName = file.FileName;
+            Extension = "." + OriginalName.Split('.')[OriginalName.Split('.').Length - 1];
+        }
+
+        public IFormFile File { get; set; }
+        public string OriginalName { get; private set; }
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string FileBaseFolder { get; set; } = @"C:\samplevideo";
+
+        public async Task WriteFile()
+        {
+            try
+            {
+                Name = DateTime.Now.Ticks + Extension;
+
+                if (!Directory.Exists(FileBaseFolder))
+                {
+                    Directory.CreateDirectory(FileBaseFolder);
+                }
+
+                var path = Path.Combine(FileBaseFolder, Name);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await File.CopyToAsync(stream);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+
+        public bool CheckIfExcelFile()
+        {
+            return (Extension == ".mp4");
+        }
+    }
+}
